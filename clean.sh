5d5b67a69d0aa613e53e9a7848e321a730fd2759
Arogya Koirala <arogyak@ras-21-64.vpn.berkeley.edu>
2022-12-22 21:01:38 +0545

    added clean.sh

---
+++ clean.sh
@@ -0,0 +1,6 @@
+# killall -u arogya
+rm -rf logs/dnq.out
+rm -rf /data/tmp/arogya/data_dec/dnq
+rm -rf /data/tmp/arogya/data_dec/outputs
+# python poppy-ready-folders.py /data/tmp/arogya/data_dec /data/tmp/arogya/data_dec/inputs/districts/districts/
+# nohup ./poppy-dnq.sh -s /data/tmp/arogya/data_dec/dnq -o /data/tmp/arogya/data_dec/outputs/predictions -y 2019 -c 25 -m /data/tmp/arogya/data/outputs/models/kmeans_3_diff_bands > logs/dnq.out &
