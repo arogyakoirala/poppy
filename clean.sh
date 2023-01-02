38c95a61b7ee9f7f7d0ad7a5dba7d1764abc69f3
Arogya Koirala <arogyak@Arogyas-MacBook-Air.local>
2023-01-02 17:42:18 +0545

    updated clean.sh

--- clean.sh
+++ clean.sh
@@ -1,8 +1,8 @@
 # killall -u arogya
-rm -rf logs/dnq.out
-rm -rf /data/tmp/arogya/data_dec/dnq
-rm -rf /data/tmp/arogya/data_dec/dnq_outputs
-# python poppy-ready-folders.py /data/tmp/arogya/data_dec /data/tmp/arogya/data_dec/inputs/districts/districts/
-# nohup ./poppy-dnq.sh -s /data/tmp/arogya/data_dec/dnq -o /data/tmp/arogya/data_dec/outputs/predictions -y 2019 -c 25 -m /data/tmp/arogya/data/outputs/models/kmeans_3_diff_bands > logs/dnq.out &
+rm -rf logs/dnq_mrs.out
+rm -rf /data/tmp/arogya/data_mls19/dnq
+rm -rf /data/tmp/arogya/data_mls19/dnq_outputs
+# python poppy-ready-folders.py /data/tmp/arogya/data_mls19 /data/tmp/arogya/data_mls19/inputs/districts/districts/
+# nohup ./poppy-dnq.sh -s /data/tmp/arogya/data_mls19/dnq -o /data/tmp/arogya/data_mls19/outputs/predictions -y 2019 -c 25 -m /data/tmp/arogya/data/outputs/models/kmeans_3_diff_bands > logs/dnq.out &
 
 
