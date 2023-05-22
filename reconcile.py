b7cc678f652116c038b9a44caf9fde3d36f234bc
Arogya Koirala <arogyakoirala@gmail.com>
2023-05-22 07:54:36 -0700

    Update reconcile.py

--- reconcile.py
+++ reconcile.py
@@ -5,11 +5,13 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("tiles_dir", help="Tiles Dir")
 parser.add_argument("pred_dir", help="Preds Dir")
+parser.add_argument("dload_dir", help="Preds Dir")
 args = parser.parse_args()
 
 
 tiles = [f.split(".gpkg")[0] for f in os.listdir(args.tiles_dir)]
 predictions = [f.split(".gpkg")[0] for f in os.listdir(args.pred_dir)]
+dloads = [f.split(".gpkg")[0] for f in os.listdir(args.dload_dir)]
 
 if len(tiles) != len(predictions):
     print(f"Length Mismatch: {len(tiles)} (tiles) vs {len(predictions)} (predictions)")
@@ -18,4 +20,13 @@ for t in tiles:
     if t in predictions:
         continue
     else:
-        print(f"Couldn't find predictions for {t} in {args.pred_dir}")
+        print(f"Couldn't find predictions for {t}")
+
+for t in tiles:
+    if t in dloads:
+        if ["NOCROP" in os.listdir(args.dload_dir + f"/{t}")]:
+            print(f"No crop data for: {t}")
+        continue
+    else:
+        print(f"Couldn't find predictions for {t}")
+
