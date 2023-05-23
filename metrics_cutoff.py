c1f2f3df69a8196dce329bc1477837abd459242e
Arogya Koirala <arogyakoirala@gmail.com>
2023-05-23 13:59:42 -0700

    Update metrics_cutoff.py

--- metrics_cutoff.py
+++ metrics_cutoff.py
@@ -92,7 +92,7 @@ folders = [f for f in folders if int(f.split("_")[0]) in subset_dict[subset] ]
 print(f"New number of folders: {len(folders)}")
 
 
-tighten = ["1608", "1905", "2308", "1906"]
+tighten = ["1905", "1903", "1904", "1608", "3106", "1607"]
 
 if os.path.exists("temp"):
     shutil.rmtree("temp")
@@ -101,11 +101,11 @@ Path("temp").mkdir(exist_ok=True, parents=True)
 
 predictions= {}
 for folder in folders:
-    # if folder.split("_")[0] in tighten:
-    print(f"Tightening for {folder}")
-    # clip(f'{preds_dir}/{folder}/scores.tif', f"inputs/afgmask65.gpkg", f"temp/{folder}.tif")
-    # else:
-    shutil.copy(f'{preds_dir}/{folder}/scores.tif', f"temp/{folder}.tif")
+    if folder.split("_")[0] in tighten:
+        print(f"Tightening for {folder}")   
+        clip(f'{preds_dir}/{folder}/scores.tif', f"inputs/afgmask65.gpkg", f"temp/{folder}.tif")
+    else:
+        shutil.copy(f'{preds_dir}/{folder}/scores.tif', f"temp/{folder}.tif")
     src = rasterio.open(f'temp/{folder}.tif')
     # src = rasterio.open(f'{preds_dir}/{folder}/scores.tif')
 
