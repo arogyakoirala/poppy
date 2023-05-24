524eef11498cdd6d83f05a1d06f631518023dd78
Arogya Koirala <arogyakoirala@gmail.com>
2023-05-24 10:43:45 -0700

    Update experiment.sh

--- experiment.sh
+++ experiment.sh
@@ -6,5 +6,9 @@ for cutoff in "${Cutoffs[@]}"; do
     for mask in "${Masks[@]}"; do
         echo "Cutoff:" $cutoff
         echo "Mask %:" $mask
+
+        python -m metrics_cutoff --pred_dir outputs/predictions/nadali-qandahar-r1-r2-2019-30day/predictions --csv inputs/poppy_1994-2021.csv  --year 2019 --cluster 0 --cutoff $cutoff --mask $mask --subset 2020_3 results2020_q3
+        python -m metrics_cutoff --pred_dir outputs/predictions/nadali-qandahar-r1-r2-2020-30day/predictions --csv inputs/poppy_1994-2021.csv  --year 2020 --cluster 1 --cutoff $cutoff --mask $mask --subset 2020_3 results2020_q3
+        python -m metrics_cutoff --pred_dir outputs/predictions/nadali-qandahar-r1-r2-2021-30day/predictions --csv inputs/poppy_1994-2021.csv  --year 2021 --cluster 2 --cutoff $cutoff --mask $mask --subset 2020_3 results2020_q3
     done
 done
