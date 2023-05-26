31a24c7a2f69c077fa36209546155ea26a4b1766
Arogya Koirala <arogyakoirala@gmail.com>
2023-05-26 10:40:08 -0700

    Fixed inconsistennt district IDS

--- experiment.sh
+++ experiment.sh
@@ -2,8 +2,8 @@
 
 source poppy-env/bin/activate
 
-declare -a Cutoffs=("0.8" "0.6" "0.4" "0.2")
-declare -a Masks=("95" "85" "75" "65")
+declare -a Cutoffs=("0" "0.2" "0.6")
+declare -a Masks=("60" "65" "70" "75" "80")
 
 
 for cutoff in "${Cutoffs[@]}"; do
@@ -11,8 +11,8 @@ for cutoff in "${Cutoffs[@]}"; do
         echo "Cutoff:" $cutoff
         echo "Mask %:" $mask
 
-        python -u metrics_cutoff.py --pred_dir outputs/predictions/nadali-qandahar-r1-r2-2019-30day/predictions --csv inputs/poppy_1994-2021.csv  --year 2019 --cluster 0 --cutoff $cutoff --mask $mask --subset 2020_3 results2020_q3
+        python -u metrics_cutoff.py --pred_dir outputs/predictions/nadali-qandahar-r1-r2-2019-30day/predictions --csv inputs/poppy_1994-2021.csv  --year 2019 --cluster 0 --cutoff $cutoff --mask $mask --subset 2019_3 results2019_q3
         python -u metrics_cutoff.py --pred_dir outputs/predictions/nadali-qandahar-r1-r2-2020-30day/predictions --csv inputs/poppy_1994-2021.csv  --year 2020 --cluster 1 --cutoff $cutoff --mask $mask --subset 2020_3 results2020_q3
-        python -u metrics_cutoff.py --pred_dir outputs/predictions/nadali-qandahar-r1-r2-2021-30day/predictions --csv inputs/poppy_1994-2021.csv  --year 2021 --cluster 2 --cutoff $cutoff --mask $mask --subset 2020_3 results2020_q3
+        python -u metrics_cutoff.py --pred_dir outputs/predictions/nadali-qandahar-r1-r2-2021-30day/predictions --csv inputs/poppy_1994-2021.csv  --year 2021 --cluster 2 --cutoff $cutoff --mask $mask --subset 2021_3 results2021_q3
     done
 done
