e1582d851b3c240b372d08dbfc14bcd40142d42b
Arogya Koirala <arogyakoirala@gmail.com>
2023-06-04 10:10:33 -0700

    Update experiment.sh

--- experiment.sh
+++ experiment.sh
@@ -2,8 +2,8 @@
 
 source poppy-env/bin/activate
 
-declare -a Cutoffs=("0.6" "0.2" "0")
-declare -a Masks=("80" "75" "70" "65" "60")
+declare -a Cutoffs=("0.0")
+declare -a Masks=("60")
 
 
 for cutoff in "${Cutoffs[@]}"; do
@@ -11,8 +11,8 @@ for cutoff in "${Cutoffs[@]}"; do
         echo "Cutoff:" $cutoff
         echo "Mask %:" $mask
 
-        python -u metrics_cutoff.py --pred_dir outputs/predictions/nadali-qandahar-r1-r2-2019-30day/predictions --csv inputs/poppy_1994-2021.csv  --year 2019 --cluster 0 --cutoff $cutoff --mask $mask --subset 2019_3 results2019_q3
-        python -u metrics_cutoff.py --pred_dir outputs/predictions/nadali-qandahar-r1-r2-2020-30day/predictions --csv inputs/poppy_1994-2021.csv  --year 2020 --cluster 1 --cutoff $cutoff --mask $mask --subset 2020_3 results2020_q3
-        python -u metrics_cutoff.py --pred_dir outputs/predictions/nadali-qandahar-r1-r2-2021-30day/predictions --csv inputs/poppy_1994-2021.csv  --year 2021 --cluster 2 --cutoff $cutoff --mask $mask --subset 2021_3 results2021_q3
+        python -u metrics_cutoff.py --pred_dir outputs/predictions/nadali-qandahar-r1-r2-2019-30day/predictions --csv inputs/poppy_1994-2021.csv  --year 2019 --cluster 0 --cutoff $cutoff --mask $mask --subset 2019_3 results2019_cameraready
+        python -u metrics_cutoff.py --pred_dir outputs/predictions/nadali-qandahar-r1-r2-2020-30day/predictions --csv inputs/poppy_1994-2021.csv  --year 2020 --cluster 1 --cutoff $cutoff --mask $mask --subset 2020_3 results2020_cameraready
+        python -u metrics_cutoff.py --pred_dir outputs/predictions/nadali-qandahar-r1-r2-2021-30day/predictions --csv inputs/poppy_1994-2021.csv  --year 2021 --cluster 2 --cutoff $cutoff --mask $mask --subset 2021_3 results2021_cameraready
     done
 done
