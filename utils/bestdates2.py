c16ed3b4f159fe9d1400be278580052f588e8790
Arogya Koirala <arogyakoirala@gmail.com>
2023-06-28 18:17:18 -0700

    Update bestdates2.py

--- utils/bestdates2.py
+++ utils/bestdates2.py
@@ -149,6 +149,7 @@ class DatesHelper:
         if len(os.listdir(TILE_DIR)) > 1:
             # Merge downloaded modis tiles into one
             raw = [f"{TILE_DIR}/{f}" for f in os.listdir(TILE_DIR)]
+            print(raw)
             vrt_options = gdal.BuildVRTOptions(resampleAlg='cubic', addAlpha=True)
             gdal.BuildVRT(f'{MODIS_DIR}/temp.vrt', raw, options=vrt_options)
             # os.system(f'find {TILE_DIR}  -maxdepth 1 -name "*.tif" -print0 | xargs -0 gdalbuildvrt -srcnodata "0" {MODIS_DIR}/temp.vrt')
